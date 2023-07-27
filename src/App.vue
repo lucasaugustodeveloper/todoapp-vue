6daf75914acc08cd77412a513516fd6431ff56bc
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-07-27 09:17:37 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld text="TodoApp Vue" />
+    </div>
+  </header>
+
+  <main>
+    main
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1>{{ text }}</h1>
+  </div>
+</template>
+
+<style scoped>
+  h1 {
+    font-size: 3rem;
+    font-weight: 300;
+  }
+</style>
